9965427298e1e590ae0688b2772eb4b1ec05f736
LeeKM321 <stephen4951@gmail.com>
2026-02-19 10:32:19 +0900

    Initial commit

---
+++ src/main/java/com/codeit/blog/BlogCachePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codeit.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCachePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCachePracticeApplication.class, args);
+    }
+
+}
